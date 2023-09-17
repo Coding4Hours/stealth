8f25134cf8e36f134e78250442c0ea512f717bdb
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-09-17 16:00:12 -0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install nodejs
 node -v
-https://github.com/ungoogled-software/ungoogled-chromium/archive/refs/tags/117.0.5938.88-1.zip
+wget https://github.com/ungoogled-software/ungoogled-chromium/archive/refs/tags/117.0.5938.88-1.zip
 git clone https://github.com/tholian-network/stealth.git;
 node ./stealth/make.mjs;
 node ./stealth/stealth.mjs serve --debug=true;
